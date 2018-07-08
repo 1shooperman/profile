66cdf986cf297af0afe45ce534e47ebc476fe293
Brandon Shoop <3355821+1shooperman@users.noreply.github.com>
2018-07-08 09:10:01 -0400

    Update profile.bash

--- profile.bash
+++ profile.bash
@@ -7,8 +7,8 @@
  
  export EDITOR="/usr/bin/vim"
  
- # clone (raw) https://github.com/git/git/blob/master/contrib/completion/git-completion.bash into ./custom
- source ./custom/sourceme.bash
+ # clone (raw) https://github.com/git/git/blob/master/contrib/completion/git-completion.bash into ./scripts
+ source ./scripts/sourceme.bash
  
 export GIT_PS1_SHOWDIRTYSTATE=1
 export PS1='\n\[\033[01;34m\]\w\n\[\033[01;32m\]\u@\h\[\033[01;33m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '
